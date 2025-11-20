532e30903237de73dc031fc4c1e655efa65f6206
NabievCoding <nabievofficial04@gmail.com>
2025-11-20 15:26:26 +0300

    fix: move autoprefixer and postcss to dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/asluzbeklugati/' // Netlify uchun kerak bo’ladi!
+  base: '/asluzbeklugati/',
 })
